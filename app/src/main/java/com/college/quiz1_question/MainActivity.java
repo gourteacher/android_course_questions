1967018ec448f7b6b4d6708c1f3f7557a1cb0dd5
gourteacher <ouaazs@algonquincollege.com>
2022-06-04 20:16:53 -0400

    Initial version

---
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -0,0 +1,23 @@
+package com.college.quiz1_question;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int mCounter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //counterView is the TextView that displays the value of the counter
+        TextView counterView = findViewById(R.id.counterId);
+
+        //TODO
+        //Your code here
+
+    }
+}
