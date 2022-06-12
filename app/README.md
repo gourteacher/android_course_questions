52638aab9ebf4b97d5e2967a84eff66fce1e1d72
gourteacher <ouaazs@algonquincollege.com>
2022-06-12 02:23:20 -0400

    Exercise - Implicit Intents Question

---
+++ app/README.md
@@ -0,0 +1,5 @@
+
+It contains three buttons for sending implicit intents:
+* Open a URL in a browser.
+* Find a location on a map.
+* Share a text string.
--- app/src/main/java/com/college/questions/MainActivity.java
+++ app/src/main/java/com/college/questions/MainActivity.java
@@ -1,31 +1,114 @@
 package com.college.questions;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
+
+
+/**
+ * The ImplicitIntents app contains three buttons for sending implicit intents:
+ * - Open a URL in a browser
+ * - Find a location on a map
+ * - Share a text string
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // EditText view for the website URI
+    private EditText mWebsiteEditText;
+    // EditText view for the location URI
+    private EditText mLocationEditText;
+    // EditText view for the share text
+    private EditText mShareTextEditText;
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // This EditText contains the user input
-        // for the number of burgers
-        EditText numberStr = findViewById(R.id.numberId);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_editext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
+    }
 
-        // This yextview displays the resut of multiplying
-        // the content of the numberId EditText by 5
-        TextView resultTv = findViewById(R.id.resultId);
+    /**
+     * Handles the onClick for the "Open Website" button.  Gets the URI
+     * from the edit text and sends an implicit intent for that URL.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void openWebsite(View view) {
+        // Get the URL text.
+        String url = mWebsiteEditText.getText().toString();
 
-        // TODO
-        // Implement the 2 listeners for buttons 'clear' and 'pay'
-        // One burger is $5
-        // Result should contain 5 multiplied by number chosen
-        // Display should be prefixed with the '$' sign such as $15
-        // Reset should clear all the fields
+        // Parse the URI and create the intent.
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Find an activity to handle the intent and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    /**
+     * Handles the onClick for the "Open Location" button.  Gets the location
+     * text from the edit text and sends an implicit intent for that location.
+     *
+     * The location text can be any searchable geographic location.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void openLocation(View view) {
+        // Get the string indicating a location.  Input is not validated; it is
+        // passed to the location handler intact.
+        String loc = mLocationEditText.getText().toString();
+
+        // Parse the location and create the intent.
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        // Find an activity to handle the intent, and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    /**
+     * Handles the onClick for the "Share This Text" button.  The
+     * implicit intent here is created by the  {@link ShareCompat.IntentBuilder}
+     * class.  An app chooser appears with the available options for sharing.
+     *
+     * ShareCompat.IntentBuilder is from the v4 Support Library.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void shareText(View view) {
+        // Get the shared text.
+        String txt = mShareTextEditText.getText().toString();
+
+        // Build the share intent with the mimetype text/plain and launch
+        // a chooser for the user to pick an app.
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType("text/plain")
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
     }
 }
