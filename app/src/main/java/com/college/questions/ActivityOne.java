ff91607dce256a03d411092d802f9f8ecb9ef6cc
gourteacher <ouaazs@algonquincollege.com>
2022-11-02 15:14:07 -0400

    Activities with Intent Question

---
+++ app/src/main/java/com/college/questions/ActivityOne.java
@@ -0,0 +1,28 @@
+package com.college.questions;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+
+ Complete the TODOs below
+
+*/
+
+public class ActivityOne extends AppCompatActivity {
+
+    static final String KEY_NAME="Name";
+    static final String KEY_RANK="Rank";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+        //TODO 1 - Create the intent here
+
+        //TODO 2 - Save information to pass
+
+        //TODO 3 - start the activity when the button is clicked
+    }
+}
---
+++ app/src/main/java/com/college/questions/ActivityTwo.java
@@ -0,0 +1,22 @@
+package com.college.questions;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        //TODO 4 - Retrieve the textview to display
+
+        //TODO 5 - Retrieve the data
+
+        //TODO 6 - Display the retrieved information on the screen
+
+        //TODO 7 - Return to previous activity when BACK button is clicked
+    }
+}
