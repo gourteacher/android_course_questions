5b22f00444cd89f21b5d7708cf6ede366d979975
gourteacher <ouaazs@algonquincollege.com>
2023-11-20 18:21:10 -0500

    Remove unused TODOs

--- app/src/main/java/com/college/quiz1_question/HelpFragment.java
+++ app/src/main/java/com/college/quiz1_question/HelpFragment.java
@@ -14,12 +14,9 @@ import androidx.fragment.app.Fragment;
  */
 public class HelpFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -35,7 +32,6 @@ public class HelpFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment HelpFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static HelpFragment newInstance(String param1, String param2) {
         HelpFragment fragment = new HelpFragment();
         Bundle args = new Bundle();
--- app/src/main/java/com/college/quiz1_question/OutboxFragment.java
+++ app/src/main/java/com/college/quiz1_question/OutboxFragment.java
@@ -14,12 +14,9 @@ import androidx.fragment.app.Fragment;
  */
 public class OutboxFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -35,7 +32,6 @@ public class OutboxFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment OutboxFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static OutboxFragment newInstance(String param1, String param2) {
         OutboxFragment fragment = new OutboxFragment();
         Bundle args = new Bundle();
